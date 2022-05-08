837c66506f4bf28e800ecaedbdcee49d3d6aaa54
Rotthin <rotthin_dev@protonmail.com>
2022-05-08 03:08:29 +0200

    Preloader minor changes

--- Scripts/Preloader.gd
+++ Scripts/Preloader.gd
@@ -1,11 +1,21 @@
 extends Node
 
+const GENERATION_1_START: int = 1
+const GENERATION_1_END: int = 151
+const GENERATION_2_START: int = 152
+const GENERATION_2_END: int = 251
+
 var http_req: HTTPRequest
 var requests: Array
 var is_busy: bool
 var start_time: int
 
 func _ready():
+	if !OS.is_debug_build():
+		printerr("Preloader can be run only in debug build")
+		queue_free()
+		return
+
 	is_busy = false
 	requests = Array()
 
@@ -14,24 +24,32 @@ func _ready():
 	add_child(http_req)
 
 	start_time = OS.get_system_time_msecs()
-	preload_generation(1, 1, 151)
-	preload_generation(2, 152, 251)
+	preload_generation(1, GENERATION_1_START, GENERATION_1_END)
+	preload_generation(2, GENERATION_2_START, GENERATION_2_END)
 
 func preload_generation(gen: int, from: int, to: int) -> void:
+	var str_gen = str(gen)
 	var dir: Directory = Directory.new()
 	if dir.open("/home/rxn/dev/godot/Pokex/Pokemons") != OK:
 		printerr("Failed to open pokemons root directory for generation %s" % gen)
 		return
 
-	if dir.dir_exists(str(gen)):
-		if dir.remove(str(gen))	!= OK:
-			printerr("Failed to remove generation directory")
+	# If directory already exists, delete the old files from it
+	if dir.dir_exists(str_gen): 
+		print("Deleting old files")
+		dir.change_dir(str_gen)
+		dir.list_dir_begin(true)
+		var file: String = dir.get_next()
+		while file != "":
+			if dir.remove(file) != OK:
+				printerr("Failed to remove file: '%s'" % file)
+			file = dir.get_next()
+	# Create the directory if it doesn't exist
+	else:
+		if dir.make_dir(str_gen) != OK:
+			printerr("Failed to make directory for generation %s" % gen)
 			return
-
-	if dir.make_dir(str(gen)) != OK:
-		printerr("Failed to make directory for generation %s" % gen)
-		return
-
+	
 	print("Preloading generation %s" % gen)
 	for i in range(from, to+1):
 		request_pokemon(i)
@@ -65,8 +83,8 @@ func on_request_completed(_result: int, _response_code: int, _headers: PoolStrin
 	data.id = json.result["id"]
 	data.name = json.result["species"]["name"]
 	data.spriteUrl = json.result["sprites"]["front_default"]
-	if data.id < 152: data.generation = 1
-	elif data.id < 252: data.generation = 2
+	if data.id <= GENERATION_1_END: data.generation = 1
+	elif data.id <= GENERATION_2_END: data.generation = 2
 	for type in json.result["types"]:
 		data.type |= PokemonData.get_type_from_string(type["type"]["name"])
 	on_load(data)
